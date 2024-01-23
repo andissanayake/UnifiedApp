145c5901ed9f42b8c5554e0719335d788698f823
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2024-01-23 17:13:05 +0800

    File Name

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -4,13 +4,13 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
-import { AxiosInterceptor } from "./app/axiosInterceptor.ts";
+import { AxiosApiInterceptor } from "./app/AxiosApiInterceptor.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <AxiosInterceptor />
+        <AxiosApiInterceptor />
         <App />
       </BrowserRouter>
     </Provider>
